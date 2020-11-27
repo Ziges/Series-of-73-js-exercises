024ea8bb746a1cbf50ccda8186c6ee7f8049e6fb
Dimitri Szygowski <dimitri.szygo@gmail.com>
2020-11-27 11:16:47 +0100

    Add solution 01-waving-text for 08-misc

--- 08-misc/01-waving-text/script.js
+++ 08-misc/01-waving-text/script.js
@@ -9,8 +9,30 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    let letterContainer = document.getElementById("target");
+    let letters = letterContainer.innerText;
+    let lettersArray = letters.split("");
+
+    letterContainer.innerText = "";
+
+    /*var i;
+    for (i = 0; i < lettersArray.length; i++) {
+        var fontsize = i % 8;
+        var result = lettersArray[i].fontsize(fontsize);
+
+        document.getElementById("target").innerHTML += result;*/
+
+    lettersArray.forEach(function (letter, i) {
+        let fontsize = i % 7;
+        console.log(fontsize);
+
+        let result = lettersArray[i].fontsize(fontsize);
+
+        document.getElementById("target").innerHTML += result;
+        console.log("loop number" + i + " for letter:" + letter);
+    })
 
 })();
+
